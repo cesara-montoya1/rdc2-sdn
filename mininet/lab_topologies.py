18d60cd88bcfc66cb99b108430885e3758dee9ef
donCESAR12345 <sprit152009@gmail.com>
2024-06-11 10:07:58 -0500

    [Feature] First laboratory, trying with custom topologies with mn command
    Signed-off-by: donCESAR12345 <sprit152009@gmail.com>

---
+++ mininet/lab_topologies.py
@@ -0,0 +1,5 @@
+from topology1 import Lab1Topo
+
+topos = {
+    "lab1": Lab1Topo,
+}
---
+++ mininet/topology1.py
@@ -0,0 +1,23 @@
+from mininet.topo import Topo
+from mininet.node import OVSSwitch
+
+
+class Lab1Topo(Topo):
+    def build(self, n=4):
+        Topo.__init__(self)
+        switches = []
+        hosts = []
+        # Leer el parámetro n
+        n = int(input("Enter the number of hosts: "))
+        for i in range(n):
+            # Añadir switch con capacidad de STP
+            swx = self.addSwitch(f"s{i+1}", cls=OVSSwitch, stp=1,
+                                 failMode="standalone")
+            # Añadir host y su enlace al switch
+            hx = self.addHost(f"h{i+1}")
+            self.addLink(swx, hx)
+            # Añadir enlaces con los demás switches
+            for sw in switches:
+                self.addLink(swx, sw)
+            switches.append(swx)
+            hosts.append(hx)
